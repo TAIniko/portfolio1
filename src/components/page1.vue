c0100d33126bb43ac2bfc1a34feeeaf39f22cd32
TAIniko <48309832+TAIniko@users.noreply.github.com>
2019-03-23 16:23:24 +0900

    Update page1.vue

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div>
     <p>
@@ -13,19 +12,3 @@
 
 <style>
 </style>
-=======
-<template>
-  <div>
-    <p>
-      ここはページ１です。
-    </p>
-    <router-link to="/page2">Go to page2</router-link>
-  </div>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
->>>>>>> 58551cfd2ed1022f5403b74850c411a39ed59f5a
